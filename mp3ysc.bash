07f55dd623037fc29408e160492d0b9c87d959b3
baixar video do youtube <156385224+jeanscnh@users.noreply.github.com>
2024-03-30 22:11:25 -0300

    Update mp3ysc.bash

--- mp3ysc.bash
+++ mp3ysc.bash
@@ -3,31 +3,10 @@
 # Função para realizar a consulta de CPF
 consultar_cpf() {
     local cpf=$1
-    local url="https://api.cpfcnpj.com.br/5f702c7c350a970b4a8a3324f9624bfc/consultar-cpf/$cpf"
+    local url="https://consulta-situacao-cpf.com/$cpf"
     local resposta=$(curl -s "$url")
     echo "$resposta"
 }
-
-# Função para exibir o resultado da consulta
-exibir_resultado() {
-    local resultado_json="$1"
-    local nome=$(echo "$resultado_json" | jq -r '.nome')
-    local situacao=$(echo "$resultado_json" | jq -r '.situacao')
-    local data=$(echo "$resultado_json" | jq -r '.data')
-    local mensagem=$(echo "$resultado_json" | jq -r '.mensagem')
-
-    echo "Nome: $nome"
-    echo "Situação: $situacao"
-    echo "Data: $data"
-    echo "Mensagem: $mensagem"
-}
-
-# Verificar se o jq está instalado
-if ! command -v jq &> /dev/null; then
-    echo "O comando jq não foi encontrado. Instalando jq..."
-    pkg install jq -y
-    clear
-fi
 
 # Função principal
 main() {
@@ -43,19 +22,19 @@ main() {
 
     resultado=$(consultar_cpf "$cpf")
 
-    if [[ -z $resultado ]]; then
-        echo "Erro ao consultar o CPF. Verifique sua conexão com a internet ou tente novamente mais tarde."
-        exit 1
-    fi
+    # Extrair informações do HTML
+    nome=$(echo "$resultado" | grep -oP '(?<=Nome: <strong>)[^<]+')
+    situacao=$(echo "$resultado" | grep -oP '(?<=Situação: <strong>)[^<]+')
+    data=$(echo "$resultado" | grep -oP '(?<=Data: <strong>)[^<]+')
+    cidade=$(echo "$resultado" | grep -oP '(?<=Cidade: <strong>)[^<]+')
+    estado=$(echo "$resultado" | grep -oP '(?<=Estado: <strong>)[^<]+')
 
-    status=$(echo "$resultado" | jq -r '.status')
-
-    if [[ $status == "OK" ]]; then
-        exibir_resultado "$resultado"
-    else
-        mensagem_erro=$(echo "$resultado" | jq -r '.mensagem')
-        echo "Erro: $mensagem_erro"
-    fi
+    # Exibir resultados
+    echo "Nome: $nome"
+    echo "Situação: $situacao"
+    echo "Data: $data"
+    echo "Cidade: $cidade"
+    echo "Estado: $estado"
 }
 
 # Chamada da função principal
